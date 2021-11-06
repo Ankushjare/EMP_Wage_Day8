2b065991b9138626b100fa9ad309e17fa47fa179
Ankushjare <jareankush@gmail.com>
2021-11-07 02:23:08 +0530

    EMP Wage Added Only 7 UC

---
+++ EMP_Wage_Day_8/EMP_Wage_Day_8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmployeeWagesComputationProgram
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            UC7 obj = new UC7();
+            obj.computeEmpWage();
+
+        }
+
+    }
+}
